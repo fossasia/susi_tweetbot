ff8f62f49e7f41acf034276435fc0121f4429d0e
Manveer695 <singhmanveer1995@gmail.com>
2017-06-21 15:39:31 +0530

    To reply back with an answer during chat

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ function TwitterBot() {
 					}
 				} 
 				else {
-					message = 'Oops, Looks like Susi is taking a break, She will be back soon' + ' - ' + new Date().toISOString().slice(new Date().toISOString().indexOf('T')+1).replace(/\..+/, '');
+					message = 'Oops, Looks like Susi is taking a break, She will be back soon' + date;
 					console.log(err);
 				}
 				console.log(message);
@@ -109,7 +109,7 @@ function TwitterBot() {
 	stream.on('direct_message', reply);
 	function reply(directMsg) {
 		console.log('You receive a message!');
-		if (directMsg.direct_message.sender_screen_name === 'susi_tweetbot') {
+		if (directMsg.direct_message.sender_screen_name === 'SusiAI1') {
 			return;
 		}
 		console.log('You receive a message!');
@@ -121,8 +121,40 @@ function TwitterBot() {
 			json: true
 		}, function (err, response, data) {
 			if (!err && response.statusCode === 200) {
-				message = data.answers[0].actions[0].expression;
-			} else {
+				if(data.answers[0].actions[1]){
+					if(data.answers[0].actions[1].type === 'rss'){
+						message += 'I found this on the web-:\n\n'
+						for(var i=0;i<((data.answers[0].metadata.count)>50?50:data.answers[0].metadata.count);i++){
+								message += ('Title : ');
+								message += data.answers[0].data[i].title+', ';
+								message += ('Link : ');
+								message += data.answers[0].data[i].link+', ';
+							message += '\n\n';
+						}
+					}
+				}
+				else{
+					if(data.answers[0].actions[0].type === 'table'){
+						var colNames = data.answers[0].actions[0].columns;
+						if((data.answers[0].metadata.count)>50)
+							message += 'Due to message limit, only some universities are shown-:\n\n';
+						else
+							message += 'Universities are shown below-:\n\n';
+						for(var i=0;i<((data.answers[0].metadata.count)>50?50:data.answers[0].metadata.count);i++){
+							for(var cN in colNames){
+								message += (colNames[cN]+' : ');
+								message += data.answers[0].data[i][cN]+', ';
+							}
+							message += '\n\n';
+						}
+					}
+					else
+					{
+						message = data.answers[0].actions[0].expression;
+					}
+				}
+			} 
+			else {
 				message = 'Oops, Looks like Susi is taking a break, She will be back soon';
 				console.log(err);
 			}
