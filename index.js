1659c7c3af9b75cd2119b46599a1df52403bd626
Manveer695 <singhmanveer1995@gmail.com>
2017-08-07 08:42:03 +0530

    Share a message with followers

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ var T = new Twit({
 
 function TwitterBot() {
 	setInterval(function() { http.get(heroku_deploy_url); }, 1800000);
-
 	var stream = T.stream('user');
+	
 	createWelcomeMessage();
 
 	stream.on('tweet', tweetEvent);
@@ -118,6 +118,7 @@ function TwitterBot() {
 		}
 		console.log('You receive a message!');
 		console.log(directMsg);
+		
 		var queryUrl = 'http://api.susi.ai/susi/chat.json?q=' + encodeURI(directMsg.direct_message.text);
 		var message = '';
 		request({
@@ -162,7 +163,7 @@ function TwitterBot() {
 				message = 'Oops, Looks like Susi is taking a break, She will be back soon';
 				console.log(err);
 			}
-			sendMessage(directMsg.direct_message.sender_screen_name, message);
+			sendEvent(directMsg.direct_message.sender.id_str, message);
 			console.log(message);
 		});
 	}
@@ -265,6 +266,54 @@ function TwitterBot() {
 				}
 			}
 		});
+	}
+
+	function sendEvent(sender,txt) {
+			var queryUrl = 'http://api.susi.ai/susi/chat.json?q='+'Share';
+			var message = '';
+			request({
+				url: queryUrl,
+				json: true
+			}, function (err, response, data) {
+				if (!err && response.statusCode === 200) {
+					message = data.answers[0].actions[0].expression;
+				}
+				else{
+					message = 'Oops, Looks like Susi is taking a break, She will be back soon';
+					console.log(err);
+				}
+				var msg = {
+	                  "event": {
+	                    "type": "message_create",
+	                    "message_create": {
+	                      "target": {
+	                        "recipient_id": sender
+	                      },
+	                      "message_data": {
+	                        "text": txt,
+	                        "ctas": [
+	                          {
+	                            "type": "web_url",
+	                            "label": "Share with your followers",
+	                            "url": "https://twitter.com/intent/tweet?text="+encodeURI("Reply by SUSI.AI - ")+encodeURI(txt)+"%0A"+encodeURI(message)+"%0Ahttps://twitter.com/SusiAI1"
+	                          }
+	                        ]
+	                      }
+	                    }
+	                  }
+	                };
+				T.post('direct_messages/events/new', msg, sent);
+
+				function sent(err, data, response) {
+					if (err) {
+						console.log('Something went wrong!');
+						console.log(err);
+					} else {
+						console.log('Event was sent!');
+					}
+				}
+			});
+			
 	}	
 }
 app.listen(app.get('port'), function() {
